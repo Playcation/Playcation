8fb4ee33018bf23df931a7273cd56c3cd1b80b7b
kangjunhyuk <kangjun1290@gmail.com>
2025-01-06 15:16:20 +0900

    Feat : 예외처리 변경
    커스텀 Exception으로 변경

--- src/main/java/com/example/playcation/game/repository/GameRepository.java
+++ src/main/java/com/example/playcation/game/repository/GameRepository.java
@@ -1,5 +1,7 @@
 package com.example.playcation.game.repository;
 
+import com.example.playcation.exception.GameErrorCode;
+import com.example.playcation.exception.GameException;
 import com.example.playcation.game.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
   default Game findByIdOrElseThrow(Long id) {
-    return findById(id).orElseThrow(() -> new RuntimeException());
+    return findById(id).orElseThrow(() -> new GameException(GameErrorCode.GAME_NOT_FOUND));
   }
 }
