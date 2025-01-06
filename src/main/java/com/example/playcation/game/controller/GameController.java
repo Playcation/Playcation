7dc6d6ea1be29e5e36c22817e010b3b1615480d5
kangjunhyuk <kangjun1290@gmail.com>
2025-01-06 15:21:00 +0900

    Feat : 게임 생성 컨트롤러 생성
    게임이 생성되었을 때 사용할 컨트롤러 생성

--- src/main/java/com/example/playcation/game/controller/GameController.java
+++ src/main/java/com/example/playcation/game/controller/GameController.java
@@ -1,10 +1,31 @@
 package com.example.playcation.game.controller;
 
+import com.example.playcation.game.Dto.CreatedGameRequestDto;
+import com.example.playcation.game.Dto.CreatedGameResponseDto;
+import com.example.playcation.game.service.GameService;
+import com.example.playcation.util.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/cards")
 public class GameController {
+
+  private final GameService gameService;
+  private JwtTokenProvider jwtTokenProvider;
+
+  @PostMapping
+  public ResponseEntity<CreatedGameResponseDto> createdCard(@RequestHeader("Authorization") String authorizationHeader, CreatedGameRequestDto requestDto) {
+    String token = authorizationHeader.replace("Bearer ", "").trim();
+    Long id = Long.parseLong(jwtTokenProvider.getUserId(token));
+    CreatedGameResponseDto responseDto = gameService.createdGame(id, requestDto);
+    return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+  }
 
 }
