f9215dd263ce718105f53984785018c83aae5987
kangjunhyuk <kangjun1290@gmail.com>
2025-01-06 19:30:11 +0900

    Feat : 새로운 예외 추가
    게임 수정,삭제시 현재 접속한 유저와 게임을 생성한 유저를 비교하였을 때 실패시 발생

--- src/main/java/com/example/playcation/exception/GameErrorCode.java
+++ src/main/java/com/example/playcation/exception/GameErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GameErrorCode {
 
-  GAME_NOT_FOUND("게임을 찾을 수 없습니다", HttpStatus.NOT_FOUND);
+  GAME_NOT_FOUND("게임을 찾을 수 없습니다", HttpStatus.NOT_FOUND),
+  DOES_NOT_MATCH("게임을 생성한 유저와 일치하지 않습니다", HttpStatus.FORBIDDEN);
 
   private final String message;
   private final HttpStatus httpStatus;
