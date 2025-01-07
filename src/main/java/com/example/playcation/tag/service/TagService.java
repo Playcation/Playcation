2f02dddd1481ede3c947ba869abc36473597e8be
kangjunhyuk <kangjun1290@gmail.com>
2025-01-07 10:29:54 +0900

    Feat : 태그 생성,검색(단건,다건) 구현
    대그의 생성과 단건 검색, 다건 검색(페이징 적용) 구현

--- src/main/java/com/example/playcation/tag/service/TagService.java
+++ src/main/java/com/example/playcation/tag/service/TagService.java
@@ -1,7 +1,14 @@
 package com.example.playcation.tag.service;
 
+import com.example.playcation.tag.Dto.CreatedTagRequestDto;
+import com.example.playcation.tag.Dto.CreatedTagResponseDto;
+import com.example.playcation.tag.entity.Tag;
 import com.example.playcation.tag.repository.TagRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +16,29 @@ import org.springframework.stereotype.Service;
 public class TagService {
 
 
+  private final TagRepository tagRepository;
+
+  public CreatedTagResponseDto CreatedTag(CreatedTagRequestDto requestDto) {
+
+    Tag tag = Tag.builder()
+        .tagName(requestDto.getTagName())
+        .build();
+
+    tagRepository.save(tag);
+    return CreatedTagResponseDto.toDto(tag);
+  }
+
+  public CreatedTagResponseDto findTag(Long tagId) {
+
+    Tag tag = tagRepository.findByIdOrElseThrow(tagId);
+
+    return CreatedTagResponseDto.toDto(tag);
+  }
+
+  public List<CreatedTagResponseDto> findAllTag(int page) {
+
+    PageRequest pageRequest = PageRequest.of(page, 10, Sort.by(Direction.DESC, "id"));
+
+    return tagRepository.findAll(pageRequest).stream().map(CreatedTagResponseDto::toDto).toList();
+  }
 }
