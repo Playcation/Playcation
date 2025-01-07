7bb8dbbc15b3ee34795e42f7e5bb92765dee23b9
kangjunhyuk <kangjun1290@gmail.com>
2025-01-07 15:32:10 +0900

    feat : 메서드 2개 추가
    자주 쓰는 findById를 default 메서드로 생성,
    클라이언트가 보내준 tagId를 가지고 있는 gameTag를 조회하는 메서드 추가

--- src/main/java/com/example/playcation/gametag/repository/GameTagRepository.java
+++ src/main/java/com/example/playcation/gametag/repository/GameTagRepository.java
@@ -1,5 +1,19 @@
 package com.example.playcation.gametag.repository;
 
-public interface GameTagRepository {
+import com.example.playcation.exception.GameTagErrorCode;
+import com.example.playcation.exception.NotFoundException;
+import com.example.playcation.gametag.entity.GameTag;
+import com.example.playcation.tag.entity.Tag;
+import java.util.List;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface GameTagRepository extends JpaRepository<GameTag, Long> {
+  default GameTag findByIdOrElseThrow(Long id) {
+    return findById(id).orElseThrow(() -> new NotFoundException(GameTagErrorCode.GAME_TAG_NOT_FOUND));
+  }
+
+  List<GameTag> findGameTagByTagId(PageRequest pageRequest, Long tagId);
 }
