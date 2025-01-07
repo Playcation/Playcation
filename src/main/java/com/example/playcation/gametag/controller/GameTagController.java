bcee591b819b600140446ddf1083b1d8b545f8cc
kangjunhyuk <kangjun1290@gmail.com>
2025-01-07 15:36:40 +0900

    fix : gameTag 생성, gameTag로 game 조회하는 기능 추가
    gameTag의 생성과 클라이언트가 보내준 tagId를 가지고 있는 gameTag의 game을 페이지네이션을 적용시켜 반환하는 기능 추가

--- src/main/java/com/example/playcation/gametag/controller/GameTagController.java
+++ src/main/java/com/example/playcation/gametag/controller/GameTagController.java
@@ -1,5 +1,39 @@
 package com.example.playcation.gametag.controller;
 
+import com.example.playcation.gametag.dto.GameListResponseDto;
+import com.example.playcation.gametag.dto.GameTagRequestDto;
+import com.example.playcation.gametag.dto.GameTagResponseDto;
+import com.example.playcation.gametag.service.GameTagService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.message.LocalizedMessageFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gameTags")
+@RequiredArgsConstructor
 public class GameTagController {
 
+  private final GameTagService gameTagService;
+
+  @PostMapping
+  public ResponseEntity<GameTagResponseDto> createdGameTag(@RequestBody GameTagRequestDto requestDto) {
+    GameTagResponseDto responseDto = gameTagService.createdGameTag(requestDto);
+    return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public ResponseEntity<GameListResponseDto> findGameTag(
+      @RequestParam(required = false) int page,
+      @RequestParam Long tagId) {
+    GameListResponseDto responseDto = gameTagService.findGameTag(page, tagId);
+    return new ResponseEntity<>(responseDto, HttpStatus.OK);
+  }
 }
--- src/main/java/com/example/playcation/gametag/service/GameTagService.java
+++ src/main/java/com/example/playcation/gametag/service/GameTagService.java
@@ -1,5 +1,59 @@
 package com.example.playcation.gametag.service;
 
+import com.example.playcation.game.entity.Game;
+import com.example.playcation.game.repository.GameRepository;
+import com.example.playcation.gametag.dto.GameListResponseDto;
+import com.example.playcation.gametag.dto.GameTagRequestDto;
+import com.example.playcation.gametag.dto.GameTagResponseDto;
+import com.example.playcation.gametag.entity.GameTag;
+import com.example.playcation.gametag.repository.GameTagRepository;
+import com.example.playcation.tag.entity.Tag;
+import com.example.playcation.tag.repository.TagRepository;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class GameTagService {
 
+  private final GameTagRepository gameTagRepository;
+  private final TagRepository tagRepository;
+  private final GameRepository gameRepository;
+
+
+  public GameTagResponseDto createdGameTag(GameTagRequestDto requestDto) {
+
+    Tag tag = tagRepository.findByIdOrElseThrow(requestDto.getTagId());
+
+    Game game = gameRepository.findByIdOrElseThrow(requestDto.getGameId());
+
+    GameTag gameTag = GameTag.builder()
+        .tag(tag)
+        .game(game)
+        .build();
+
+    gameTagRepository.save(gameTag);
+
+    return GameTagResponseDto.toDto(gameTag);
+  }
+
+
+  public GameListResponseDto findGameTag(int page, Long tagId) {
+
+    PageRequest pageRequest = PageRequest.of(page, 10, Sort.by(Direction.DESC, "id"));
+
+    List<GameTag> gameTagList = gameTagRepository.findGameTagByTagId(pageRequest, tagId).stream().toList();
+
+    List<Game> gameList = new ArrayList<>();
+    for (GameTag gameTag : gameTagList) {
+      gameList.add(gameTag.getGame());
+    }
+
+    return new GameListResponseDto(gameList);
+  }
 }
