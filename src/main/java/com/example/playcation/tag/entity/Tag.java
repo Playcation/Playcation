ee4f4244e9703d87ba7b32f2d960a59938885612
kangjunhyuk <kangjun1290@gmail.com>
2025-01-07 10:27:14 +0900

    Feat : 어노테이션 및 생성자 추가
    어노테이션 4종 및 생성자와 업데이트 메서드 추가

--- src/main/java/com/example/playcation/tag/entity/Tag.java
+++ src/main/java/com/example/playcation/tag/entity/Tag.java
@@ -4,12 +4,32 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@Table(name = "tag")
 public class Tag {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String tagName;
+
+  public Tag(Long id, String tagName) {
+    this.id = id;
+    this.tagName = tagName;
+  }
+
+  public void updateTag(String tagName) {
+    this.tagName = tagName;
+  }
 }
