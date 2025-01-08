1d4659913d0d8869242d75e69b15606616f780bf
K-da-fire <105974458+K-da-fire@users.noreply.github.com>
2025-01-08 14:41:10 +0900

    fetch : 시큐리티 admin 권한 설정

--- src/main/java/com/example/playcation/user/controller/UserController.java
+++ src/main/java/com/example/playcation/user/controller/UserController.java
@@ -20,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -94,8 +95,7 @@ public class UserController {
   // ADMIN
   @PutMapping("/{userId}/update/role")
   public ResponseEntity<UserResponseDto> updateUserRole(
-      @RequestHeader("Authorization") String authorizationHeader,
-      @RequestParam Long userId
+      @PathVariable Long userId
   ){
     return ResponseEntity.ok().body(userService.updateUserAuth(userId));
   }
