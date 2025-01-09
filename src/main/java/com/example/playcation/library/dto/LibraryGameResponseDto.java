1464fce2dc1ef64fcf551bb90717162b53257e8e
kangjunhyuk <kangjun1290@gmail.com>
2025-01-09 15:54:06 +0900

    Fix : 라이브러리 다건 조회시 문제 해결
    라이브러리 다건 조회시 유저 정보까지 같이 조회되던 문제 해결

--- src/main/java/com/example/playcation/library/dto/LibraryGameResponseDto.java
+++ src/main/java/com/example/playcation/library/dto/LibraryGameResponseDto.java
@@ -1,19 +1,21 @@
 package com.example.playcation.library.dto;
 
+import com.example.playcation.game.dto.CreatedGameResponseDto;
 import com.example.playcation.game.entity.Game;
+import java.util.List;
 import lombok.Getter;
 
 @Getter
 public class LibraryGameResponseDto {
 
-  private Game game;
+  private CreatedGameResponseDto game;
 
   private Boolean isFavourite;
 
   private Long count;
 
   public LibraryGameResponseDto(Game game, Boolean isFavourite, Long count) {
-    this.game = game;
+    this.game = CreatedGameResponseDto.toDto(game);
     this.isFavourite = isFavourite;
     this.count = count;
   }
