81bc9d650b012ee2d3801a99db099f6b64af7b96
gxnie <wlsdl7520@naver.com>
2025-01-22 22:44:08 +0900

    comment: 주석추가

--- src/main/java/com/example/playcation/emailsender/dto/EmailCheckDto.java
+++ src/main/java/com/example/playcation/emailsender/dto/EmailCheckDto.java
@@ -1,12 +1,14 @@
 package com.example.playcation.emailsender.dto;
 
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
 public class EmailCheckDto {
+
+  // 사용자가 인증번호를 확인하고 인증번호를 입력하였을 때 받아오는 DTO
+
   @Email
   @NotEmpty(message = "이메일을 입력해 주세요")
   private String email;
