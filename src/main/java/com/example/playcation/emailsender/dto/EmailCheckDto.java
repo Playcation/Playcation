10b995210f854e3b9055f4c64d4d0ef7d717bc97
gxnie <wlsdl7520@naver.com>
2025-01-23 14:17:43 +0900

    fix: @Email 삭제, 정규식 추가

--- src/main/java/com/example/playcation/emailsender/dto/EmailCheckDto.java
+++ src/main/java/com/example/playcation/emailsender/dto/EmailCheckDto.java
@@ -1,19 +1,16 @@
 package com.example.playcation.emailsender.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class EmailCheckDto {
-
   // 사용자가 인증번호를 확인하고 인증번호를 입력하였을 때 받아오는 DTO
 
-  @Email
-  @NotEmpty(message = "이메일을 입력해 주세요")
+  @NotBlank(message = "이메일은 필수값입니다.")
   private String email;
 
-  @NotEmpty(message = "인증 번호를 입력해 주세요")
+  @NotBlank(message = "인증 번호를 입력해 주세요")
   private String authNum;
 
   }
