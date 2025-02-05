a6e49b9532e17a87a96ed96441b23a46c753a662
KM4HS <ac4hsu@gmail.com>
2025-02-05 22:34:42 +0900

    feat: VIP 책정과 혜택 지급 배치 추가
    매월 1일마다 수행하도록 scheduled 설정해둠. 배포시 주석 해제 필요!

--- src/main/java/com/example/playcation/user/repository/PointRepository.java
+++ src/main/java/com/example/playcation/user/repository/PointRepository.java
@@ -1,17 +1,24 @@
 package com.example.playcation.user.repository;
 
-import ch.qos.logback.core.util.OptionHelper;
+import com.example.playcation.batch.PointWithUserDto;
 import com.example.playcation.exception.NotFoundException;
 import com.example.playcation.exception.PaymentErrorCode;
 import com.example.playcation.user.entity.Point;
-import com.example.playcation.user.entity.User;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PointRepository extends JpaRepository<Point, Long> {
 
   Optional<Point> getPointByUserId(Long userId);
-  default Point getPointByUserIdOrElseThrow(Long id){
-    return getPointByUserId(id).orElseThrow(() -> new NotFoundException(PaymentErrorCode.NOT_FOUND_POINT));
+
+  default Point getPointByUserIdOrElseThrow(Long id) {
+    return getPointByUserId(id).orElseThrow(
+        () -> new NotFoundException(PaymentErrorCode.NOT_FOUND_POINT));
   }
+
+  @Query("SELECT new com.example.playcation.batch.PointWithUserDto(p, p.user) FROM Point p WHERE p.user.name IS NOT NULL")
+  Page<PointWithUserDto> findAllPointAndUser(Pageable pageable);
 }
