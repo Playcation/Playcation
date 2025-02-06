5bffc3a164ad9eb69f451cee2047bf618bb2fdf1
JHabin <habin424@gmail.com>
2025-02-06 11:18:04 +0900

    refactor : coupon 동시성 코드 수정

--- src/main/java/com/example/playcation/coupon/service/CouponUserLockService.java
+++ src/main/java/com/example/playcation/coupon/service/CouponUserLockService.java
@@ -1,8 +1,9 @@
 package com.example.playcation.coupon.service;
 
 import com.example.playcation.coupon.repository.RedisCouponRepository;
+import com.example.playcation.exception.CouponErrorCode;
+import com.example.playcation.exception.OutOfStockException;
 import com.example.playcation.redis.distributedLock.DistributedLock;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,14 +15,16 @@ public class CouponUserLockService {
 
   private final RedisCouponRepository redisCouponRepository;
 
-  @Transactional
+  @DistributedLock(key = "T(java.lang.String).format('coupon:count:%s', #couponName)")
   public void requestCoupon(Long userId, String couponName) {
-
-    redisCouponRepository.findUserFromQueue(userId, couponName);
-    if (redisCouponRepository.getRemainingCouponCount(couponName) > 0) {
-      addQueue(userId, couponName);
-      updateCouponCount(couponName);
+    log.info("재고:{}", redisCouponRepository.getRemainingCouponCount(couponName));
+    if (redisCouponRepository.getRemainingCouponCount(couponName) <= 0) {
+      throw new OutOfStockException(CouponErrorCode.COUPON_OUT_OF_STOCK);
     }
+    redisCouponRepository.findUserFromQueue(userId, couponName);
+    addQueue(userId, couponName);
+    redisCouponRepository.decrementAndGetCouponCount(couponName);
+
   }
 
   @DistributedLock(key = "T(java.lang.String).format('COUPON:%s', #couponName)")
@@ -29,8 +32,5 @@ public class CouponUserLockService {
     redisCouponRepository.addUser(userId, couponName);
   }
 
-  @DistributedLock(key = "T(java.lang.String).format('COUPON:%s', #couponName)")
-  public void updateCouponCount(String couponName) {
-    redisCouponRepository.decrementCouponCount(couponName);
-  }
+
 }
--- src/main/java/com/example/playcation/exception/CouponErrorCode.java
+++ src/main/java/com/example/playcation/exception/CouponErrorCode.java
@@ -12,6 +12,7 @@ public enum CouponErrorCode implements ExceptionType {
   NO_AUTHORIZED_COUPON("ADMIN은 발급받을 수 없습니다.", HttpStatus.FORBIDDEN),
   DUPLICATE_ISSUED_COUPON("이미 해당 사용자에게 발급한 쿠폰입니다.", HttpStatus.BAD_REQUEST),
   DUPLICATED_REQUESTED_COUPON("이미 쿠폰을 요청하였습니다.", HttpStatus.BAD_REQUEST),
+  REQUEST_USER_NOT_FOUND("요청한 사용자가 없습니다.", HttpStatus.NOT_FOUND),
   DUPLICATE_COUPON("이미 존재하는 쿠폰입니다.", HttpStatus.BAD_REQUEST);
 
   private final String message;
