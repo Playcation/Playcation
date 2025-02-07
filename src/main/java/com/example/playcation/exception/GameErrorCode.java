6868f558b04a11813480cc339aa036caca27d429
gxnie <wlsdl7520@naver.com>
2025-02-07 11:37:22 +0900

    fix: 오타 수정

--- src/main/java/com/example/playcation/exception/GameErrorCode.java
+++ src/main/java/com/example/playcation/exception/GameErrorCode.java
@@ -10,7 +10,7 @@ public enum GameErrorCode implements ExceptionType{
 
   GAME_NOT_FOUND("게임을 찾을 수 없습니다", HttpStatus.NOT_FOUND),
   DOES_NOT_MATCH("게임을 생성한 유저와 일치하지 않습니다", HttpStatus.FORBIDDEN),
-  DUPLICATE_GAME_TITLE("이미 해당 게임이 존재압니다", HttpStatus.BAD_REQUEST),
+  DUPLICATE_GAME_TITLE("이미 해당 게임이 존재합니다", HttpStatus.BAD_REQUEST),
   REVIEW_GAME_NOT_FOUND("리뷰와 연결된 게임을 찾을 수 없습니다.",HttpStatus.NOT_FOUND);
 
   private final String message;
